f6a9d281759199dca49a7a2ab217b3a23ee2b057
pena6911-ship-it <pena6911@gmail.com>
2026-08-12 14:42:30 -0400

    build(site): add Eleventy pipeline rendering src/ to public/

---
+++ eleventy.config.js
@@ -0,0 +1,18 @@
+export default function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy('src/css');
+  eleventyConfig.addPassthroughCopy('src/js');
+  eleventyConfig.addPassthroughCopy('src/fonts');
+  eleventyConfig.addPassthroughCopy('src/img');
+
+  return {
+    dir: {
+      input: 'src',
+      output: 'public',
+      includes: '_includes',
+      data: '_data',
+    },
+    markdownTemplateEngine: 'njk',
+    htmlTemplateEngine: 'njk',
+    templateFormats: ['njk', 'md', 'html'],
+  };
+}
